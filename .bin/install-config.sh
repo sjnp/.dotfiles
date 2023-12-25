34d5913363d113c374514addcb2a678e66d31b82
sjnp <supawet.jnp@gmail.com>
2023-12-25 19:32:06 +0700

    Add installing and removing script

---
+++ .bin/install-config.sh
@@ -0,0 +1,22 @@
+git clone --bare git@github.com:sjnp/.dotfiles.git $HOME/.dotfiles
+
+gitConfig () {
+	/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+if gitConfig checkout
+then
+	echo "Getting configs... done"
+fi
+
+if gitConfig config status.showUntrackedFiles no
+then
+	echo "Setting untracked files... done"
+fi
+
+# Update .zshrc
+if echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
+then
+    echo "Seting dotfiles alias... done"
+fi
+
---
+++ .bin/remove-config.sh
@@ -0,0 +1,29 @@
+if rm -rf $HOME/.dotfiles
+then
+	echo "Removing .dotfiles directory... done"
+fi
+
+if rm -rf $HOME/.bin
+then
+	echo "Removing .bin directory... done"
+fi
+
+if rm $HOME/.config/nvim/init.lua
+then
+	echo "Removing neovim config... done"
+fi
+
+if rm $HOME/.config/tmux/tmux.conf
+then
+	echo "Removing tmux config... done"
+fi
+
+if rm $HOME/LICENSE && rm $HOME/README.md
+then
+	echo "Removing license and readme files... done"
+fi
+
+if sed -i.bak "/alias dotfiles=/d" $HOME/.zshrc
+then
+	echo "Removing dotfiles alias from .zshrc... done"
+fi
