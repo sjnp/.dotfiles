7a172f16a699b48c0edba55fed402d3015b59562
sjnp <supawet.jnp@gmail.com>
2024-05-09 09:17:34 +0700

    Add wezterm config for macOS

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,9 +2,21 @@ local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local config = wezterm.config_builder()
 
+-- Helper fn
+function IsMacOS()
+    local f = assert(io.popen('uname -a', 'r'))
+    local s = assert(f:read('*a'))
+    f:close()
+    if string.find(s, 'Darwin') then
+        return true
+    else
+        return false
+    end
+end
+
 -- Start up
 wezterm.on('gui-startup', function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
+    local _, _, window = mux.spawn_window(cmd or {})
     window:gui_window():maximize()
 end)
 
@@ -18,7 +30,9 @@ config.font = wezterm.font 'MesloLGS NF'
 config.font_size = 10.0
 
 -- Window
-config.window_decorations = 'NONE'
+if not IsMacOS() then
+    config.window_decorations = 'NONE'
+end
 config.enable_tab_bar = false
 config.enable_scroll_bar = false
 config.window_padding = {
