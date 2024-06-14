b036346846d53efb4363b638c65116232a29b3c4
sjnp <supawet.jnp@gmail.com>
2024-06-14 20:33:53 +0700

    Add wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,31 +1,30 @@
-local wezterm = require 'wezterm'
-local mux = wezterm.mux
+local wezterm = require("wezterm")
+-- local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 -- Start up
-wezterm.on('gui-startup', function(cmd)
-    local _, _, window = mux.spawn_window(cmd or {})
-    window:gui_window():maximize()
-end)
+-- wezterm.on("gui-startup", function(cmd)
+-- 	local _, _, window = mux.spawn_window(cmd or {})
+-- 	window:gui_window():maximize()
+-- end)
 
-config.term = 'wezterm'
+config.term = "wezterm"
 
 -- Colors
-config.color_scheme = 'tokyonight_night'
+config.color_scheme = "tokyonight_night"
 
 -- Font
-config.font = wezterm.font 'MesloLGS NF'
+config.font = wezterm.font("MesloLGS NF")
 config.font_size = 10.0
 
 -- Window
-config.window_decorations = 'NONE'
 config.enable_tab_bar = false
 config.enable_scroll_bar = false
 config.window_padding = {
-    left = 1,
-    right = 0,
-    top = 1,
-    bottom = 1
+	left = 1,
+	right = 0,
+	top = 1,
+	bottom = 1,
 }
 
 return config
